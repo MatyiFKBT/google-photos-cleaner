1b4ae06e5c5f7174a6081fc26cf0b6e37ae3f9db
MatyiFKBT <6183867+MatyiFKBT@users.noreply.github.com>
2023-10-31 16:09:21 +0100

    chore: list all monthly photos (#4)

--- apps/web/pages/dashboard.tsx
+++ apps/web/pages/dashboard.tsx
@@ -1,20 +1,18 @@
 import React from 'react'
-import { trpc } from '../utils/trpc'
 import { Layout } from '@/components/layout'
 import DashboardAddItem from '@/components/dashboard/new-item'
 import DashboardListItems from '@/components/dashboard/list-items'
+import DashboardListMonthItems from '@/components/dashboard/list-month-items'
 
-
-// TODO: revamp this page
 
 function Dashboard() {
 	return (
 		<Layout>
 			<h1>
-
 				Dashboard
 			</h1>
 			<DashboardListItems />
+			<DashboardListMonthItems />
 			<DashboardAddItem />
 		</Layout>
 
--- packages/api/index.ts
+++ packages/api/index.ts
@@ -2,4 +2,3 @@ export { appRouter, type AppRouter } from "./src/routers/_app";
 
 export { createContext } from "./src/context";
 export type { Context } from "./src/context";
-// export { include } from './src/routers/entry'
